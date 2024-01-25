4ae79bad3d23fd39029758935f23887ff6ee34d4
ilveron <alessandromonetti@outlook.it>
2024-01-25 17:53:04 +0100

    Update propic and portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -8,20 +8,23 @@ import {Project} from "../../models/project.model";
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
-  university!: Array<Project>
+  universityBachelor!: Array<Project>
+  universityMaster!: Array<Project>
   school!: Array<Project>
   personal!: Array<Project>
 
   constructor(private requestService: RequestService) { }
 
   ngOnInit(): void {
-    this.university = new Array<Project>()
+    this.universityMaster = new Array<Project>()
+    this.universityBachelor = new Array<Project>()
     this.school = new Array<Project>()
     this.personal = new Array<Project>()
 
     this.requestService.getPortfolio().subscribe({
       next: (response: any) => {
-        this.university = response.university
+        this.universityMaster = response.universityMaster
+        this.universityBachelor = response.universityBachelor
         this.school = response.school
         this.personal = response.personal
 
