c5ae998336a663c7a4b02c17f54919950c1bdd98
ilveron <alessandromonetti@outlook.it>
2025-03-24 13:32:52 +0100

    Update info

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
@@ -7,19 +7,55 @@ import {AfterViewInit, Component} from '@angular/core';
 })
 export class HomepageComponent implements AfterViewInit {
 
-  charPos = 0
+  charPos = 0;
+  positionIndex = 0;
   fullName = "Alessandro Monetti";
-
+  positions: string[] = [
+    "Computer Security University Student",
+    "Photographer",
+    "Tech Passionate"
+  ];
+  currentPosition = "";
+  positionTransitioning = false;
+  positionTimeout = 0;
 
 
   ngAfterViewInit(): void {
-    this.typewriterEffect()
+    setTimeout(() => this.changePosition(), this.positionTimeout); // Start after name animation
+    this.typewriterEffect();
   }
 
   private typewriterEffect() {
-    if(this.charPos >= 0 && this.charPos < this.fullName.length){
+    if (this.charPos >= 0 && this.charPos < this.fullName.length) {
       document.getElementById("fullName")!.innerHTML += this.fullName.charAt(this.charPos++);
-      setTimeout(() => this.typewriterEffect(), 50)
+      setTimeout(() => this.typewriterEffect(), 50);
     }
+  }
+
+  private changePosition() {
+    if (this.positionTimeout === 0) {
+        this.positionTimeout = 3000
+    }
+    if (this.positionTransitioning) return;
+
+    this.positionTransitioning = true;
+
+    const positionElement = document.getElementById("position")!;
+    positionElement.classList.add('slide-out'); // Trigger animation
+
+    setTimeout(() => {
+      this.positionIndex = (this.positionIndex + 1) % this.positions.length;
+      this.currentPosition = this.positions[this.positionIndex];
+      positionElement.classList.remove('slide-out');
+      positionElement.classList.add('slide-in');
+
+
+      setTimeout(() => {
+        positionElement.classList.remove('slide-in');
+        this.positionTransitioning = false;
+        setTimeout(() => this.changePosition(), 3000); // Change every 3 seconds
+      }, 500); // Match transition duration
+
+    }, 500); // Match transition duration
   }
 }
